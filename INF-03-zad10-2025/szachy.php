6c0e3816bf9034a5973a0f3ad20bd3d7663056c4
Tomasz Wyczesany <37375539+tomaszwyczesany@users.noreply.github.com>
2025-02-10 10:14:31 +0100

    zm
    zm

--- INF-03-zad10-2025/szachy.php
+++ INF-03-zad10-2025/szachy.php
@@ -11,7 +11,7 @@
         <h2>Koło szachowe gambit piona</h2>
     </header>
     <main>
-        <section id="lewy">
+        <section id="left-block">
             <h4>Polecane linki</h4>
             <ul>
                 <li><a href="kw1.png">kwerenda1</a></li>
@@ -21,8 +21,9 @@
             </ul>
             <img src="logo.png" alt="Logo koła szachowego">
         </section>
-        <section id="prawy">
+        <section id="right-block">
             <h3>Najlepsi gracze naszego koła</h3>
+			
             <table>
                 <thead>
                     <tr>
@@ -34,23 +35,6 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Wypełnione przez Skrypt 1 
-                    SELECT pseudonim, tytul, ranking, klasa FROM zawodnicy WHERE ranking > 2787 ORDER BY ranking DESC;
-                    SELECT pseudonim, klasa FROM zawodnicy ORDER BY RAND() LIMIT 2;
-                    UPDATE zawodnicy SET klasa='5A' WHERE klasa='4A';
-                    SELECT pseudonim, data_zdobycia, DATEDIFF(CURRENT_DATE, data_zdobycia) AS 'dni' FROM zawodnicy WHERE tytul != '';
-                    
-                    Logo 320px na 320px logo.png 
-                    Dwa pliki plansza i pionki musilismy wysiac tło nałożyć na siebie wykadrować wyskalować i nałożyć jedno na drugie
-                    
-                    PHP CSS 
-                    Header z h2
-                    Aside z listą ul z przekierowaniami do ss z sql
-                    Section h4 "Najlepszi gracze naszego koła" pod tabelka(PHP) i paragraf
-                    Fotter Strone wykonał 
-                    
-                    jeszcze tu macie php kod 
-                    -->
                             <?php
                                 $conn=mysqli_connect('localhost', 'root', '', 'szachy');
                                 $zapytanie="SELECT pseudonim, tytul, ranking, klasa FROM zawodnicy WHERE ranking > 2787 ORDER BY ranking DESC";
@@ -70,9 +54,27 @@
                             ?>
                 </tbody>
             </table>
-            <form method="post" action="skrypt2.php">
-                <button type="submit">Losuj nową parę graczy</button>
-            </form>
+            <form method="post" action="">
+					<button type="submit" name = "submit" >Losuj nową parę graczy</button>
+					<?php
+						if (isset($_POST["submit"]))
+						{
+							//echo"<div>test</div>";
+							$conn=mysqli_connect('localhost', 'root', '', 'szachy');
+							$zapytanie="SELECT pseudonim, klasa FROM zawodnicy ORDER BY RAND() LIMIT 2;";
+							$wynik=mysqli_query($conn,$zapytanie);
+							 echo "<h4>";
+							while($row=mysqli_fetch_row($wynik))
+							{
+								echo "<span> $row[0] $row[1] </span>";
+							}
+							 
+							 echo "</h4>";
+							 mysqli_close($conn);
+						}							
+					?>
+			</form>
+			<p>Legenda: AM - Absolutny Mistrz, SM - Szkolny Mistrz, PM - Mistrz Poziomu, KM - Mistrz Klasowy</p>
         </section>
     </main>
     <footer>
